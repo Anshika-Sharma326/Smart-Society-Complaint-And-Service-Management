05455f7716b3194bdfef07b80387a76ad5fcf7c6
Anshika-Sharma326 <anshikasharma0914@gmail.com>
2026-09-09 12:15:19 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://smart-society-backend-7vzn.onrender.com/api",
 });
 
 export default api;
